ff2552d8e0d126c7777735a7fae3df4ef6bec12f
Rafaela9136 <rafaela9136@gmail.com>
2016-10-30 21:47:16 -0200

    Changing AquaWorld scenes

--- Aqua/Assets/Scripts/Screens/Aqua/AquaMatch/GlobalConsts.cs
+++ Aqua/Assets/Scripts/Screens/Aqua/AquaMatch/GlobalConsts.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class GlobalConsts {
     
     //scenes name
-    public const string SCENE_AQUA_MAP = "AquaHome";
+    public const string SCENE_AQUA_MAP = "AquaWorld";
     public const string SCENE_LOADING = "Loading";
     public const string SCENE_HOME = "Match";
     public const string SCENE_MAP_SELECT = "MapSelect";
---
+++ Aqua/Assets/Scripts/Screens/Aqua/AquaWorld.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AquaWorld : MonoBehaviour {
+
+	public AudioSource musicPlayer;
+	public ParticleSystem bubblesLeft;
+	public ParticleSystem bubblesRigth;
+
+	// Use this for initialization
+	void Start () {
+		bubblesLeft.GetComponent<ParticleSystem>().playbackSpeed = 0.15f;
+		bubblesRigth.GetComponent<ParticleSystem>().playbackSpeed = 0.15f;
+
+		bubblesLeft.Play();
+		bubblesRigth.Play();
+	}
+	
+	void OpenGame(string scene) {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		DontDestroyOnLoad(musicPlayer);
+		
+		if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name == "AquaWorld")
+        {
+        	musicPlayer.Stop();
+            SceneManager.LoadScene("Home");
+        }
+	}
+}
