106c248e3a615279211d791f777ce8a9f2375856
Rafaela9136 <rafaela9136@gmail.com>
2016-11-28 23:28:03 -0200

    Adding scenes for groups

---
+++ Aqua/Assets/Scripts/Screens/Groups.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Groups : GenericScreen 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		backScene = "Home";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public void SelectGroup(Text groupName)
+	{
+	}
+}
---
+++ Aqua/Assets/Scripts/Screens/ManageGroup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ManageGroup : GenericScreen {
+
+	// Use this for initialization
+	void Start () 
+	{
+		backScene = "Groups";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void CreateGroup(Text groupName) 
+	{
+	}
+
+	public void AddMembers(Text member)
+	{
+	}
+}
